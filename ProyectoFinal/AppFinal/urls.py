f21c2e22b866c6cb9e3e5626158bf8758a34e694
lacattaneo <lacattaneo@uade.edu.ar>
2024-08-23 16:08:49 -0300

    herencias

--- ProyectoFinal/AppFinal/urls.py
+++ ProyectoFinal/AppFinal/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from AppFinal.views import index,aboutCorrientes,exploraCorrientes
+from AppFinal.views import index,about,post, contact, padre
 
 urlpatterns = [
     path('', index, name='index'),
-    path('aboutCorrientes/', aboutCorrientes, name='aboutCorrientes'),
-    path('exploraCorrientes/', exploraCorrientes, name='exploraCorrientes')
+    path('about/', about, name='about'),
+    path('post/', post  , name='post'),
+    path('contact/', contact,name='contact'),
+    path('padre/', padre, name="padre")
     
 ]
--- ProyectoFinal/AppFinal/views.py
+++ ProyectoFinal/AppFinal/views.py
@@ -6,9 +6,15 @@ def index(request):
     return render(request, 'index.html')
 
 
-def aboutCorrientes(request):
-    return render(request, 'appFinal/about.html')
+def about(request):
+    return render(request, 'about.html')
 
 
-def exploraCorrientes(request):
-    return render(request, 'ppFinal/post.html')
+def post(request):
+    return render(request, 'post.html')
+
+def contact (request):
+    return render(request, 'contact.html')
+
+def padre(request):
+    return render(request,'padre.html')
